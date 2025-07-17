fe424244429d755d6fceb9d4e8652e70a8a60bf2
Emerson Recto <emersontrecto@gmail.com>
2025-07-16 21:26:51 -0400

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  // routes here
   return (
     <>
       <h1>Fullstack University</h1>
+      <Routes>
+        <Route index element="" />
+        <Route path="/departments" element="" />
+      </Routes>
     </>
   );
 }
