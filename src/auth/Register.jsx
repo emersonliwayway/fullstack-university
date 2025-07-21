a83248f7f5b2b66d33097a1f2f6c846574b56f74
Emerson Recto <emersontrecto@gmail.com>
2025-07-21 14:45:20 -0400

    completed auth context

---
+++ src/auth/Register.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
+
+/** A form that allows users to register for a new account */
+export default function Register() {
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(null);
+
+  const tryRegister = async (formData) => {
+    const firstname = formData.get("firstname");
+    const lastname = formData.get("lastname");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
+      if (firstname && lastname) {
+        await register({ firstname, lastname, email, password });
+      } else {
+        await register({ email, password });
+      }
+      //   navigate to home page, edit for specific route
+      //   navigate("/home");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  //   edit for specific project, originally from book buddies
+  //   ***note classnames
+
+  return (
+    <>
+      <h1>Register for an account</h1>
+      <form action={tryRegister} className="loginForm">
+        <label className="firstname">
+          First name
+          <input type="text" name="firstname" required />
+        </label>
+        <label className="lastname">
+          Last name
+          <input type="text" name="lastname" required />
+        </label>
+        <label className="email">
+          Email
+          <input type="text" name="email" required />
+        </label>
+        <label className="password">
+          Password
+          <input type="password" name="password" required />
+        </label>
+        <button className="registerButton">Register</button>
+        {error && <output>{error}</output>}
+        <a onClick={() => navigate("/login")}>
+          Already have an account? Login here.
+        </a>
+      </form>
+    </>
+  );
+}
