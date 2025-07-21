5ba834a019622027b1e5e660eac6dca7643efe45
Emerson Recto <emersontrecto@gmail.com>
2025-07-21 19:35:25 -0400

    dummy data files

---
+++ src/dummy data/faculty.js
@@ -0,0 +1,22 @@
+const faculty = [
+  {
+    id: 1,
+    fname: "Yoshi",
+    lname: "Recto",
+    bio: "Yoshi is a dog and a professor.",
+    profile: "imgUrl",
+    phone: "333-333-3333",
+    email: "yoshi@fsu.com",
+  },
+  {
+    id: 2,
+    fname: "Bosco",
+    lname: "Recto",
+    bio: "Bosco is a professor and a dog.",
+    profile: "imgUrl",
+    phone: "444-444-4444",
+    email: "bosco@fsu.com",
+  },
+];
+
+export default faculty;
