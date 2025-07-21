948ae651d1cbe78213abaf412d49a928ac4ee6f1
Emerson Recto <emersontrecto@gmail.com>
2025-07-21 15:07:20 -0400

    router pages

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <strong>
+          <p>Fullstack University</p>
+        </strong>
+
+        <nav>
+          <a href="">Departments</a>
+          <a href="">Faculty</a>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "./auth/AuthContext.jsx";
+import { ApiProvider } from "./api/ApiContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <ApiProvider>
+        <BrowserRouter>
+          <Layout>
+            <App />
+          </Layout>
+        </BrowserRouter>
+      </ApiProvider>
+    </AuthProvider>
+  </StrictMode>
+);
